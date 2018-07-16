24049130d47f1c71d5ed1c6fc5a1ee9ae7045798
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-16 08:42:50 -0600

    initial commit of PHP snap-sum challenge

---
+++ phpsnap-sum.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: judeamiller
+ * Date: 7/16/18
+ * Time: 8:23 AM
+ */
+
+
+
+$randomNumbers = array(1, 2, 3, 4, 5);
+echo "arraySum ="  .array_sum($randomNumbers);
+
+?>
